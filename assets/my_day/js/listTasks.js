80a30e64cc947cfff902aed4b04d04890acfe916
Qqkyu <kulmisiu007@gmail.com>
2021-02-17 00:49:39 +0100

    Every card in my-day-task-container now has its own id with task id in it; added transition for removing task

--- assets/my_day/js/listTasks.js
+++ assets/my_day/js/listTasks.js
@@ -5,6 +5,21 @@ function createTime(hour, minute) {
     return time;
 }
 
+function myDayMarkTaskAsDone(id) {
+    markAsDone(id);
+    fillSideCardData(true);
+    listMyDayTasks();
+}
+
+function myDayRemoveTask(id) {
+    document.getElementById(`my-day-task-${id}`).classList.add("scale-out");
+    setTimeout(function() {
+        removeTask(id);
+        fillSideCardData(true);
+        listMyDayTasks();
+    }, 500);
+}
+
 function createMyDayContainerHTML(tasks) {
     let HTML = "", description, time;
     for(let i = 0; i < tasks.length; ++i) {
@@ -21,7 +36,7 @@ function createMyDayContainerHTML(tasks) {
                 time = createTime(tasks[i]['timeHour'], tasks[i]['timeMinute']);
             }
         }
-        HTML += `<div class="card blue-grey darken-1 hoverable">` +
+        HTML += `<div class="card blue-grey darken-1 hoverable scale-transition" id="my-day-task-${tasks[i]['id']}">` +
                 `  <div class="card-content white-text side-card">` +
                 `    <p class="card-main-text"><i class="small material-icons left">description</i>${description}</p>`;
         if(timeSpecified) {
@@ -29,13 +44,13 @@ function createMyDayContainerHTML(tasks) {
         }
         HTML +=   `  </div>` +
                   `  <div class="card-action side-card">` +
-                  `    <button class="waves-effect waves-light btn" onclick="markAsDone(${tasks[i]['id']}); fillSideCardData(true); listMyDayTasks();">` +
+                  `    <button class="waves-effect waves-light btn" onclick="myDayMarkTaskAsDone(${tasks[i]['id']});">` +
                   `      <i class="material-icons right">done</i>Mark as done` +
                   `    </button>` +
                   `    <button class="waves-effect waves-light btn">` +
                   `      <i class="material-icons right">edit</i>Edit` +
                   `    </button>` +
-                  `    <button class="waves-effect waves-light btn" onclick="removeTask(${tasks[i]['id']}); fillSideCardData(true); listMyDayTasks();">` +
+                  `    <button class="waves-effect waves-light btn" onclick="myDayRemoveTask(${tasks[i]['id']});">` +
                   `      <i class="material-icons right">clear</i>Remove` +
                   `    </button>` +
                   `  </div>` +
